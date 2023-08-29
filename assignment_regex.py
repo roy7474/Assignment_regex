d3f1f67634f1a61c6242e37efff7a4e61dd38fc4
Roy Contreras Benitez <roy7474@gmail.com>
2023-08-28 20:58:24 -0400

    program has been finished and tested. Try and except were added

--- assignment_regex.py
+++ assignment_regex.py
@@ -6,36 +6,31 @@
 # actual data: There are 100 values and the sum ends with 804
 
 
-#fhandle = open('sample_data.txt')
+# The program is unnescessarily long, it has been done to practice my knowledge on functions and list
 
-def number_extractor(data):
+def number_extractor(data):                     # Define the function
     import re                   
-    all_digits_str = list()
+    all_digits_str = list()                     # Define the lists
     all_digits = list()
-    #open(data)
-    for line in data:
+
+    for line in data:                           # loop to extract all digits
         words = line.rstrip()
         x = re.findall('[0-9]+', words)
         all_digits_str += x
-    for val in all_digits_str:
+    for val in all_digits_str:                  # Convert the digits from strings to intergers
         val = int(val)
-        all_digits.append(val)
+        all_digits.append(val)                  # Add digits to a list
     return all_digits
     
-# print('The sum of all', len(all_digits), 'values in the sample data is:', sum(all_digits))
-results = number_extractor(open('sample_data.txt'))
 
 
+file_name = input('Enter file name: ')          # Get the name of the file
+
+try: 
+    file2 = open(file_name)
+except FileNotFoundError:
+    print('File could not be found. Please try again!')
+    quit()
+
+results = number_extractor(file2)
 print('The sum of all', len(results), 'values in the sample data is:', sum(results))
-'''
-#ope file directly for esier testing of the program
-fhandle = open('sample_data.txt')
-for line in fhandle:
-    words = line.rstrip()
-    x = re.findall('[0-9]+', words)
-    all_digits_str += x
-for val in all_digits_str:
-    val = int(val)
-    all_digits.append(val)
-    
-print('The sum of all', len(all_digits), 'values in the sample data is:', sum(all_digits))'''
